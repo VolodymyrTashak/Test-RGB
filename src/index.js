bf8f119c6970861efe54764c53f192c598e0aacb
VolodymyrTashak <vovavozny33@gmail.com>
2023-06-12 19:23:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,20 +9,16 @@ form.addEventListener('submit', function(event) {
         const phoneNumber = document.getElementById("number").value;
         
         const formattedNumber = countryCode + phoneNumber;
-        
-        console.log("Форматований номер: " + formattedNumber);
     
         const params = {
-            // toName: document.getElementById('toName').value,
             fromName: document.getElementById('fromName').value,
             email: email,
             number: formattedNumber
-            // number: document.getElementById('number').value
-            // message: document.getElementById('message').value
         };
 
         emailjs.send('service_08dnbzq', 'template_zomzxcr', params)
           .then(function(response) {
+            alert("Запис пройшов успішно");
             console.log('Email sent successfully:', response.status, response.text);
           }, function(error) {
             console.log('Error sending email:', error);
